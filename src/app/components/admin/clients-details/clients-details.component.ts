6fbc926564728218704f78896db687b271b6960f
Brian Sunday <brians931@gmail.com>
2020-01-31 15:00:40 +0300

    setup dependencies for the details componeent as well intiliaze a service to fetch a particulars client details

--- src/app/components/admin/clients-details/clients-details.component.ts
+++ src/app/components/admin/clients-details/clients-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from 'src/app/services/client.service';
+import { Client } from '../../../models/Clients';
+import { Router, ActivatedRoute , Params} from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
 
 @Component({
   selector: 'app-clients-details',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clients-details.component.css']
 })
 export class ClientsDetailsComponent implements OnInit {
+  id: string;
+  client: Client;
+  hasBalance = false;
+  showBalanceUpdateInput = false;
 
-  constructor() { }
+  constructor(
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flash: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+    // Get id from ur
+    this.id = this.route.snapshot.params['id'];
+
+    // Get the client associated with the id
+
+    this.clientService.getClients(this.id).subscribe(client => {
+      this.client = client;
+    });
+
   }
 
 }
