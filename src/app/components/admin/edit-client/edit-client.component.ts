11a6857d045593436e6e22166a35b2f55d7f8044
Brian Sunday <brians931@gmail.com>
2020-02-01 14:23:53 +0300

    add method to fetch a client details

--- src/app/components/admin/edit-client/edit-client.component.ts
+++ src/app/components/admin/edit-client/edit-client.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from 'src/app/services/client.service';
+import { Client } from '../../../models/Clients';
+import { Router, ActivatedRoute , Params} from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 
 @Component({
   selector: 'app-edit-client',
@@ -6,10 +11,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-client.component.css']
 })
 export class EditClientComponent implements OnInit {
+  id: string;
+  client: Client = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    balance: 0,
+    businessLocation: '',
+    businessName: '',
+    isActive: false,
+    isVendor: true
+  };
+  clientRef: any;
+  disableBalanceOnEdit = true;
 
-  constructor() { }
+  constructor(
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flash: FlashMessagesService,
+    private db: AngularFireDatabase
+  ) { }
 
   ngOnInit() {
+    // Get id from ur
+    this.id = this.route.snapshot.params['id'];
+
+    // Get the client associated with the id
+    this.getClient(this.id);
+
+  }
+
+  getClient(id: string) {
+    this.clientRef = this.db.object('/users/clients/' + id);
+
+    this.clientRef.snapshotChanges().subscribe(action => {
+      const data = action.payload.val() as Client;
+      data.key = action.key;
+
+      this.client = data;
+
+    });
+
+  }
+
+  onSubmit({value, valid}: {value: Client, valid: booleab}) {
+    
   }
 
 }
