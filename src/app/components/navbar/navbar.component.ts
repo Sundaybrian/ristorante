d58f510bb60890f23f80f780138bcfabae549595
Brian Sunday <brians931@gmail.com>
2020-02-02 00:08:27 +0300

    update logout method to redirect to login on successfull logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean;
+  currentUser: string;
+  showRegister: boolean;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private flash: FlashMessagesService
+
+  ) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(
+      auth => {
+        if (auth) {
+         this.isLoggedIn = true;
+         this.currentUser = auth.email;
+        } else {
+          this.isLoggedIn = false;
+        }
+      }
+    );
+  }
+
+  onLogoutClick() {
+    // method to logout a user
+    this.authService.logout();
+    this.flash.show('You are now logged out', {
+      cssClass: 'alert-success', timeout: 4000
+    });
+    this.router.navigate(['/login']);
   }
 
 }
