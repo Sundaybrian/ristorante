fb4928bab95fa649bdaa1d9e614e29341e7e617d
Brian Sunday <brians931@gmail.com>
2020-02-02 00:46:46 +0300

    add auth guard to the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,20 +7,22 @@ import { AddClientComponent } from './components/admin/add-client/add-client.com
 import { EditClientComponent } from './components/admin/edit-client/edit-client.component';
 import { ClientsDetailsComponent } from './components/admin/clients-details/clients-details.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AuthGuard } from './guards/auth.guard';
 
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent},
+  { path: '', component: DashboardComponent, canActivate: [AuthGuard]},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-  { path: 'client/add', component: AddClientComponent},
-  { path: 'client/edit/:id', component: EditClientComponent},
-  { path: 'client/:id', component: ClientsDetailsComponent},
+  { path: 'client/add', component: AddClientComponent , canActivate: [AuthGuard]},
+  { path: 'client/edit/:id', component: EditClientComponent, canActivate: [AuthGuard]},
+  { path: 'client/:id', component: ClientsDetailsComponent , canActivate: [AuthGuard]},
   { path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
