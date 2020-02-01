4e7affc453bf918c87d0a308362ce99a9728fdfe
Brian Sunday <brians931@gmail.com>
2020-02-01 22:47:30 +0300

    createa login method in the auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth,
+
+  ) { }
+
+  login(email: string, password: string) {
+    return new Promise((resolve, reject) => {
+      this.afAuth.auth.signInWithEmailAndPassword(email, password).then( userData => resolve(userData),
+      err => reject(err));
+    });
+  }
 }
