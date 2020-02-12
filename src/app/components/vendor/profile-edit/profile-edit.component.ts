f4f1ae13d55b552e50b23ce229b405738f62d394
Brian Sunday <brians931@gmail.com>
2020-02-12 17:15:39 +0300

    add method to fetch the vendor obj from firebase

--- src/app/components/vendor/profile-edit/profile-edit.component.ts
+++ src/app/components/vendor/profile-edit/profile-edit.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Client } from 'src/app/models/Clients';
+import { ClientService } from 'src/app/services/client.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Subscription } from 'rxjs';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-profile-edit',
   templateUrl: './profile-edit.component.html',
   styleUrls: ['./profile-edit.component.css']
 })
-export class ProfileEditComponent implements OnInit {
+export class ProfileEditComponent implements OnInit,OnDestroy {
+  id: string;
+  client: Partial<Client> = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    balance: 0,
+    businessLocation: '',
+    businessName: '',
+    isActive: false,
+    isVendor: true
+  };
 
-  constructor() { }
+  client$: Subscription;
 
-  ngOnInit(): void {
+  constructor(
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flash: FlashMessagesService,
+    private db: AngularFireDatabase
+  ) {
+    // Get id from url
+    this.id = this.route.snapshot.params['id'];
+
+    // get the client associated with the id
+    this.client$ = this.db.object('/users/clients/' + this.id).snapshotChanges().subscribe(
+      action => {
+        const data = action.payload.val() as Client;
+        this.client = data;
+      }
+    );
+   }
+
+  ngOnInit() {
+
+  }
+
+  ngOnDestroy() {
+    this.client$.unsubscribe();
   }
 
 }
